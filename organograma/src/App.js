76c5ba897b22467f082401a0634320f03146cd35
Victor Miguel <victormfmedeiros@hotmail.com>
2025-05-14 22:08:45 -0300

    Update

--- organograma/src/App.js
+++ organograma/src/App.js
@@ -47,7 +47,6 @@ function App() {
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
   }
 
@@ -56,7 +55,7 @@ function App() {
       <Banner />
       <Formulario  times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
       
-      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
+      {times.map(time => <Time colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
     </div>
   );
 }
